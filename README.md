0ea2637853548e072b25040435b7c881d6f18063
UnmarkedBanshee <dxp38@pct.edu>
2022-03-23 21:36:20 -0400

    gui_file added
    started working on desktop application file and polished up script file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Universal-Clipboard
  Saves copied data system wide and stores it to be accessed later. Allows users to paste all previouly copied data with "paste ALT" functionality. Made with TLC
+
+ #updating this readme from the mac laptop
--- get_data_script.py
+++ get_data_script.py
@@ -1,2 +1,25 @@
+import clipboard
+import sys
 
-print('Hello')
+# 3 different allowed commands
+if len(sys.argv) == 2:
+    command = sys.argv[1] #the command passed in after the script name, use it to do different things
+    if command == 'save':
+        pass
+    elif command == 'load':
+        pass
+    elif command == 'items':
+        pass
+    else:
+        print('Invalid command')
+else:
+    print('Invalid number of arguments')
+
+
+ls = []
+if clipboard.paste() != clipboard.paste():
+    ls.append(clipboard.paste())
+data_dictionary = {}
+data_dictionary[len(data_dictionary)] = clipboard.paste()
+print(data_dictionary)
+print(sys.argv)
